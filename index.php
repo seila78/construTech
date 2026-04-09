5daae5e65c39cb33db081b1fcba63d00361dd479
seila32 <vitoroliveiradesa9@gmail.com>
2026-04-09 16:07:23 -0300

    Refactor code structure for improved readability and maintainability

--- index.php
+++ index.php
@@ -10,5 +10,28 @@
     <?php
         require 'partials/header.php';
     ?>
+    <main class="container">
+        <div class="login-texto">
+            <h1>Entre no sistema:</h1>
+            <img src="./imagens/Design sem nome.png" alt="Login">
+        </div>
+
+        <div class="formulario">
+                <h2>entre em contato com a gente!</h2>
+                <p>Nome completo:</p>
+                <input type="text" placeholder="Nome completo"> 
+                <br>
+                <p>E-mail:</p>
+                <input type="email" placeholder="E-mail"> 
+                <br>
+                <p>Telefone:</p>
+                <input type="number" placeholder="Telefone"> 
+                <br>
+                <p>Assunto:</p>
+                <input type="text" placeholder="Assunto" class="assunto"> 
+                <br>
+                <button type="submit" class="btn-formulario">enviar</button>
+            </div>
+    </main>
 </body>
 </html>
