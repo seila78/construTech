77f57e42aa4fda027818a94947e7878a3a937f1e
seila32 <vitoroliveiradesa9@gmail.com>
2026-04-09 14:59:31 -0300

    Add CSS styles and update logo path in index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ConstruTech</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <header>
         <nav>  
-            <img src="assets/logo.png" alt="Logo da ConstruTech" class="logo">
+            <img src="./imagens/logo.png" alt="Logo da ConstruTech" class="logo">
         </nav>
     </header>
 </body>
