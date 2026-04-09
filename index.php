9b1decd723ad9c45dad0e9d070d632452fb94250
seila32 <vitoroliveiradesa9@gmail.com>
2026-04-09 16:35:06 -0300

    Update form layout and styles for login functionality

--- index.php
+++ index.php
@@ -17,18 +17,12 @@
         </div>
 
         <div class="formulario">
-                <h2>entre em contato com a gente!</h2>
-                <p>Nome completo:</p>
-                <input type="text" placeholder="Nome completo"> 
-                <br>
+                <h2>entre no sistema!</h2>
                 <p>E-mail:</p>
                 <input type="email" placeholder="E-mail"> 
                 <br>
-                <p>Telefone:</p>
-                <input type="number" placeholder="Telefone"> 
-                <br>
-                <p>Assunto:</p>
-                <input type="text" placeholder="Assunto" class="assunto"> 
+                <p>Senha:</p>
+                <input type="password" placeholder="Senha"> 
                 <br>
                 <button type="submit" class="btn-formulario">enviar</button>
             </div>
