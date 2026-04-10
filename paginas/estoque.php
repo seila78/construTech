47cf357fee0acbba5e3301e44ff715d70f5b1e6c
seila32 <vitoroliveiradesa9@gmail.com>
2026-04-10 16:19:31 -0300

    Add estoque page and corresponding CSS styles

---
+++ paginas/estoque.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ConstruTech</title>
+    <link rel="stylesheet" href="../CSS/estoque.css">
+</head>
+<body>
+    <?php
+        require '../partials/header2.php';
+    ?>
+    
+    <table>
+
+    </table>
+
+    <button class="button">
+    <svg 
+        class="svg" 
+        xmlns="http://www.w3.org/2000/svg" 
+        height="25px" 
+        viewBox="0 -960 960 960" 
+        width="25px" 
+        fill="#000000"
+    >
+        <path d="M480-160L160-480l56-56 224 224v-488h80v488l224-224 56 56-320 320Z"></path>
+    </svg>
+</button>
+</body>
+</html>
