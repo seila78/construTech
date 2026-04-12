60662ba0f906b43837a38a5feb285e4fe53f5ea5
lvzangrande <zangrande360@gmail.com>
2026-04-12 17:34:23 -0300

    Adicionei a função de saudação personalizada conforme o horário e finalizei o backend da página de login

--- php/data.php
+++ php/data.php
@@ -6,11 +6,34 @@ $_SESSION['usuarios']=
 $usuarios =[
     [
     'id'    => 1,
-    'nome'  => 'admin'
+    'nome'  => 'admin',
     'senha' => '123'
     ]
 ];
 
+$nome = $_POST['user'] ?? '';
+$senha = $_POST['senha'] ?? '';
+
+$acesso = false;
+
+foreach ($usuarios as $user) {
+    if ($user['nome'] === $nome && $user['senha'] === $senha) {
+        $acesso = true;
+        break;
+    }
+}
+
+if ($acesso) {
+    header("Location: ../paginas/home.php");
+    exit;
+} else {
+    $_SESSION['erro'] = "Usuário ou senha errado!";
+    header("Location: ../index.php");
+    exit;
+}
+
+
+//estoque
 $_SESSION['produtos']= 
 $produtos =[
 [
@@ -139,4 +162,3 @@ if (!isset($_SESSION['produtos'])) {
     $_SESSION['produtos'] = $produtos
     ;
 }
-?>
