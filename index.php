cdf17e1200fe6e42fe36bb8414c94aee1e5fe39c
LUCAS VIEIRA ZANGRANDE <25254212@educ123.sp.senai.br>
2026-04-14 14:05:53 -0300

    criei o arquivo da página do financeiro

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="formulario">
-            <form action='php/data.php' method='POST'>
+            <form action='sdata.php' method='POST'>
             <h2>ENTRE NO SISTEMA!</h2>
             <p>Nome de usuário:</p>
             <input type="text" placeholder="E-mail" name='user'> 
