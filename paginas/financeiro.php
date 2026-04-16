6c0d19309e4d5e4644b95ff818cf48baaeb4a90f
seila32 <vitoroliveiradesa9@gmail.com>
2026-04-16 17:03:17 -0300

    Add financeiro.php and corresponding CSS for financial overview page

---
+++ paginas/financeiro.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ConstruTech</title>
+    <link rel="stylesheet" href="../CSS/financeiro.css">
+</head>
+<body>
+    <?php
+        require '../partials/header2.php';
+    ?>
+    <main class="container">
+        <div class="container-tabela">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Produto</th>
+                        <th>Tipo</th>
+                        <th>Quantidade</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Cimento CP II (50kg)</strong></td>
+                        <td><span class="categoria">Bruto</span></td>
+                        <td class="preco">R$ 32,90</td>
+                        <td class="preco">R$ 32,90</td>
+                    </tr>
+                    
+
+                </tbody>
+            </table>
+        </div>
+
+        <div class="lado-direito">
+            <a href="./estoque.php">
+                <div class="card">
+                    <h2>0</h2>
+                    <h2>Estoque</h2>
+                </div>
+            </a>
+
+            <div class="card">
+                <h2>R$ 152,90</h2>
+                <h2>Financeiro</h2>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
