8faab5df9db113626e39e88a8e7a5a14e3909670
seila32 <vitoroliveiradesa9@gmail.com>
2026-04-22 16:53:03 -0300

    Refactor financeiro.php and datafinanceiro.php to enhance financial transaction handling and display; implement dynamic data rendering for transactions

--- paginas/financeiro.php
+++ paginas/financeiro.php
@@ -10,6 +10,7 @@
 <body>
     <?php
         require '../partials/header2.php';
+        require_once '../php/datafinanceiro.php'; 
     ?>
     <main class="container">
         <div class="container-tabela">
@@ -23,221 +24,59 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="200" height="200">
-                                <polygon points="50,10 90,50 65,50 65,90 35,90 35,50 10,50" fill="#4CAF50" />
-                            </svg>
-                        </td>
-                        <td class="preco">13</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="200" height="200">
-                                <polygon points="35,10 65,10 65,50 90,50 50,90 10,50 35,50" fill="#F44336" />
-                            </svg>
-                        </td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Cimento CP II (50kg)</strong></td>
-                        <td><span class="categoria">Bruto</span></td>
-                        <td class="preco">R$ 32,90</td>
-                        <td class="preco">R$ 32,90</td>
-                    </tr>
+                    <?php
+                    if (isset($_SESSION['extrato']) && is_array($_SESSION['extrato'])) {
+                        foreach ($_SESSION['extrato'] as $transacao) {
+                            
+                            if ($transacao['tipo'] === 'venda') {
+                                $icone = '
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                                    <polygon points="50,10 90,50 65,50 65,90 35,90 35,50 10,50" fill="#4CAF50" />
+                                </svg>';
+                            } else {
+                                $icone = '
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                                    <polygon points="35,10 65,10 65,50 90,50 50,90 10,50 35,50" fill="#F44336" />
+                                </svg>';
+                            }
+
+                            $valorFormatado = 'R$ ' . number_format($transacao['valor'], 2, ',', '.');
+
+                            echo '
+                            <tr>
+                                <td><strong>' . htmlspecialchars($transacao['produto']) . '</strong></td>
+                                <td>' . $icone . '</td>
+                                <td class="preco">' . htmlspecialchars($transacao['quantidade']) . '</td>
+                                <td class="preco">' . $valorFormatado . '</td>
+                            </tr>';
+                        }
+                    } else {
+                        echo '<tr><td colspan="4" style="text-align:center;">Nenhum dado financeiro encontrado.</td></tr>';
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
 
         <div class="lado-direito">
             <div class="card-lucro">
-                <h2><span>-R$ 152,90</span><br>Lucro</h2>
+                <?php 
+                    $lucroFormatado = 'R$ ' . number_format($subtotal, 2, ',', '.');
+                ?>
+                <h2><span><?php echo $lucroFormatado; ?></span><br>Saldo</h2>
             </div>
-                <div class="card">
-                    <h2><span>0</span><br>pedidos pendentes</h2>
-                    <a href="./pendente.php">
-                        <button class="botao">Ver pedidos</button>
-                    </a>
-                </div>
-            
-                <div class="card">
-                    <h2><span>0</span><br>Baixo estoque</h2>
-                    <a href="./baixo_estoque.php">
-                        <button class="botao">Solicitar fornecedor</button>
-                    </a>
-                </div>
+            <div class="card">
+                <h2><span>0</span><br>pedidos pendentes</h2>
+                <a href="./pendente.php">
+                    <button class="botao">Ver pedidos</button>
+                </a>
+            </div>
+            <div class="card">
+                <h2><span>0</span><br>Baixo estoque</h2>
+                <a href="./baixo_estoque.php">
+                    <button class="botao">Solicitar fornecedor</button>
+                </a>
+            </div>
         </div>
     </main>
 </body>
--- php/datafinanceiro.php
+++ php/datafinanceiro.php
@@ -1,41 +1,224 @@
 <?php
+session_start();
 //---------------------------------------FINANCEIRO--------------------------------------------
 
 $tipos = [
     'compra' => 'Compra',
-    'venda'  => 'venda'
-];//fazer filtro dps
+    'venda'  => 'Venda'
+];
 
-//$_POST[''];
-
-
-$_SESSION['extrato'] = 
-    $extrato = [
-        [
-        /*  'id'        =>,
-            'valor'     =>,
-            'tipo'      =>,
-            'data'      =>
-            'produto'   =>
-            'quantidade =>'*/
-        ]
-    ];
-
-if (empty($extrato['tipo'] < 0)){
-    $extrato['tipo'] = 'compra'
-;}
-elseif(empty($extrato['tipo'] )){
-    $extrato['tipo'] = 'venda'
-;}
-
+$_SESSION['extrato'] = $extrato = [
+    [
+        'id'         => 1,
+        'valor'      => 329.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-18',
+        'produto'    => 'Cimento CP II (50kg)',
+        'quantidade' => 10
+    ],
+    [
+        'id'         => 2,
+        'valor'      => 39.90,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-19',
+        'produto'    => 'Martelo de Unha 27mm',
+        'quantidade' => 1
+    ],
+    [
+        'id'         => 3,
+        'valor'      => 1318.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-20',
+        'produto'    => 'Porcelanato Polido 60x60cm',
+        'quantidade' => 20
+    ],
+    [
+        'id'         => 4,
+        'valor'      => 85.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-21',
+        'produto'    => 'Torneira de Bancada (Cozinha)',
+        'quantidade' => 1
+    ],
+    [
+        'id'         => 5,
+        'valor'      => 120.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-22',
+        'produto'    => 'Areia Média',
+        'quantidade' => 1
+    ],
+    [
+        'id'         => 6,
+        'valor'      => 698.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-06',
+        'produto'    => 'Vaso Sanitário com Caixa',
+        'quantidade' => 2
+    ],
+    [
+        'id'         => 7,
+        'valor'      => 1645.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-06',
+        'produto'    => 'Cimento CP II (50kg)',
+        'quantidade' => 50
+    ],
+    [
+        'id'         => 8,
+        'valor'      => 95.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-07',
+        'produto'    => 'Lâmpada LED 9W',
+        'quantidade' => 10
+    ],
+    [
+        'id'         => 9,
+        'valor'      => 66.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-08',
+        'produto'    => 'Trena de 5 metros',
+        'quantidade' => 3
+    ],
+    [
+        'id'         => 10,
+        'valor'      => 950.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-09',
+        'produto'    => 'Tijolo Baiano (8 furos)',
+        'quantidade' => 1000
+    ],
+    [
+        'id'         => 11,
+        'valor'      => 550.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-10',
+        'produto'    => 'Pedra Brita 1',
+        'quantidade' => 5
+    ],
+    [
+        'id'         => 12,
+        'valor'      => 910.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-10',
+        'produto'    => 'Argamassa ACIII (20kg)',
+        'quantidade' => 20
+    ],
+    [
+        'id'         => 13,
+        'valor'      => 249.90,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-11',
+        'produto'    => 'Furadeira de Impacto 700W',
+        'quantidade' => 1
+    ],
+    [
+        'id'         => 14,
+        'valor'      => 57.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-12',
+        'produto'    => 'Alicate Universal 8"',
+        'quantidade' => 2
+    ],
+    [
+        'id'         => 15,
+        'valor'      => 900.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-13',
+        'produto'    => 'Carrinho de Mão (65L)',
+        'quantidade' => 5
+    ],
+    [
+        'id'         => 16,
+        'valor'      => 540.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-14',
+        'produto'    => 'Tinta Acrílica Branca (18L)',
+        'quantidade' => 3
+    ],
+    [
+        'id'         => 17,
+        'valor'      => 1200.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-15',
+        'produto'    => 'Areia Média',
+        'quantidade' => 10
+    ],
+    [
+        'id'         => 18,
+        'valor'      => 988.50,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-16',
+        'produto'    => 'Porcelanato Polido 60x60cm',
+        'quantidade' => 15
+    ],
+    [
+        'id'         => 19,
+        'valor'      => 170.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-17',
+        'produto'    => 'Torneira de Bancada (Cozinha)',
+        'quantidade' => 2
+    ],
+    [
+        'id'         => 20,
+        'valor'      => 199.50,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-18',
+        'produto'    => 'Martelo de Unha 27mm',
+        'quantidade' => 5
+    ],
+    [
+        'id'         => 21,
+        'valor'      => 493.50,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-19',
+        'produto'    => 'Cimento CP II (50kg)',
+        'quantidade' => 15
+    ],
+    [
+        'id'         => 22,
+        'valor'      => 475.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-20',
+        'produto'    => 'Tijolo Baiano (8 furos)',
+        'quantidade' => 500
+    ],
+    [
+        'id'         => 23,
+        'valor'      => 749.70,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-20',
+        'produto'    => 'Furadeira de Impacto 700W',
+        'quantidade' => 3
+    ],
+    [
+        'id'         => 24,
+        'valor'      => 285.00,
+        'tipo'       => 'compra',
+        'data'       => '2026-04-21',
+        'produto'    => 'Lâmpada LED 9W',
+        'quantidade' => 30
+    ],
+    [
+        'id'         => 25,
+        'valor'      => 349.00,
+        'tipo'       => 'venda',
+        'data'       => '2026-04-22',
+        'produto'    => 'Vaso Sanitário com Caixa',
+        'quantidade' => 1
+    ]
+];
 
 $subtotal = 0;
 
 foreach($extrato as $transacao){
- $subtotal += $transacao['valor'];
+    $tipo = isset($transacao['tipo']) && in_array($transacao['tipo'], array_keys($tipos)) ? $transacao['tipo'] : 'venda';
+
+    if ($tipo === 'compra') {
+        $subtotal -= $transacao['valor'];
+    } else {
+        $subtotal += $transacao['valor'];
+    }
 }
-
-//solicitar produtos ao fornecedor
-/*$comprar = $_POST['produto'];*/
-
 ?>
